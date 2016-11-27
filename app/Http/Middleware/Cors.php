b759470700fb961c25906185139dad4efc2b677e
Azlain Saavedra <servicomputacion2@gmail.com>
2016-11-27 11:37:49 -0400

    Update Cors.php

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,7 +15,7 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        $domains = ['http://0.0.0.0:3333'];
+        $domains = ['https://yeomandist.herokuapp.com'];
 
         if(isset($request->server()['HTTP_ORIGIN'])){
             $origin = $request->server()['HTTP_ORIGIN'];
